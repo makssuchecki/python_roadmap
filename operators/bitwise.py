1ea82b94abc50a4f568b8f13ef75a94881104aa0
makssuchecki <maks.suchecki@gmail.com>
2026-08-25 20:55:55 +0200

    operators

---
+++ operators/bitwise.py
@@ -0,0 +1,19 @@
+# Bitwise operators are used to compare (binary) numbers
+
+print(6 & 3)
+
+#  0110 (6)
+#  0011 (3)
+# =0010 (2)
+ 
+print(6 | 3)
+
+#  0110
+#  0011
+# =0111
+
+print(6 ^ 3)
+
+#  0110
+#  0011
+# =0101
---
+++ operators/precedence.py
@@ -0,0 +1,5 @@
+# Parentheses has the highest precedence
+print((6 + 3) - (6 - 3))
+
+# Multiplication has higher precedence than addition +
+print(100 + 5 * 3)
