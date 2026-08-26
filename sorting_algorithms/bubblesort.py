267431de8581d974d60a87a2572b6204f182d322
Maks Suchecki <maks.suchecki@gmail.com>
2026-08-26 15:52:49 +0200

    bubble sort

--- sorting_algorithms/bubblesort.py
+++ sorting_algorithms/bubblesort.py
@@ -1,10 +1,9 @@
-def bubblesort(list):
-    for iter_num in range(len(list)-1, 0, -1):
-        for idx in range(iter_num):
-            if list[idx]>list[idx+1]:
-                temp = list[idx]
-                list[idx] = list[idx]
-                list[idx+1] = temp
+def bubblesort(lst):
+    n = len(lst)
+    for i in range(n):
+        for j in range(n - i - 1):
+            if lst[j] > lst[j+1]:
+                lst[j], lst[j+1] = lst[j+1], lst[j]
 list = [19,2,31,45,6,11,121,27]
 bubblesort(list)
 print(list)
