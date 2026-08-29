25b522697a484c062b625c64bb7d519b3a424920
makssuchecki <maks.suchecki@gmail.com>
2026-08-29 16:49:53 +0200

    custom modules

---
+++ modules/custom.py
@@ -0,0 +1,11 @@
+import example
+
+print(example.add(4, 5))
+
+import example as ex
+
+print(ex.bubblesort([4, 6, 1, 9, 7, 3]))
+
+from example import merge_sort
+
+print(example.merge_sort([7, 2, 9, 4, 1, 4, 3]))
